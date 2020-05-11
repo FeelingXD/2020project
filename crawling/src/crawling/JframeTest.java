c64a253524f0a7e839cfb1a758b293fda89b1501
FeelingXD <wlals425315@gmail.com>
2020-05-11 23:03:01 +0900

    11

--- crawling/src/crawling/JframeTest.java
+++ crawling/src/crawling/JframeTest.java
@@ -1,43 +1,23 @@
 package crawling;
 
-import java.io.IOException;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class JframeTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-}
-public Document executelogin(String user , String password) {
-	try {
-		Connection.Response loginFoarm = Jsoup.connect("http://elearn.gwangju.ac.kr/index.jsp")
-				.method(Connection.Method.GET)
-				.execute();
-		
-		Document mainPage = (Document) Jsoup.connect("http://elearn.gwangju.ac.kr/index.jsp")
-			.data("user_id", user)
-			.data("passwd", password)
-			.cookies(loginForm()).execute()
-			.followRedirects(True)
-			.method(Connection.Method.POST).execute();
-		
-		System.out.println(mainPage.parse());
-		
-		Document evaluationPage = Jsoup.connect("https://backoffice.holidayinsider.com/backoffice2/")
-	               .cookies(cookies)
-	               .get();
-	           System.out.println(evaluationPage);
-		
-		
-			
-	}catch (IOException e) {
-		
-		e.printStackTrace();
-	}
+	  public static void main(String[] args) {
+	     
+		  Frame f = new Frame("정보 입력");
+		  f.setLayout(new FlowLayout());
+		  f.addWindowListener(new WindowAdapter() {
+			  public void windowClosing(WindowEvent e) {
+				  System.exit(0);
+			  }
+		});
+		  
+	    }
 
 
 	
