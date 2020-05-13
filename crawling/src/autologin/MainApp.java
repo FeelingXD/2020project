94774646a4156a905c03cfb20a07102f1f971946
FeelingXD <wlals425315@gmail.com>
2020-05-13 21:32:26 +0900

    13

--- crawling/src/autologin/MainApp.java
+++ crawling/src/autologin/MainApp.java
@@ -1,23 +1,43 @@
 package autologin;
 
 import autologin.Tester;
+
+import java.io.IOException;
+
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class MainApp {
 	public static void main(String args[])
 	{
-		Tester(); //인터넷 연결체크
 		
-	}
+		try
+	       {
+	           Response initialResponse;
 
-	private boolean isConnectedNetwork() {
-	boolean networkState = false;
-	
-	
-	return networkState;
-	
-	
-	
+	           initialResponse = Jsoup.connect("http://elearn.gwangju.ac.kr/index.jsp")
+	                   .execute();
+
+	           Response loginResponse = Jsoup
+	                   .connect("http://elearn.gwangju.ac.kr/index.jsp")
+	                   .cookies(initialResponse.cookies())
+	                   .data("user_id", "20174222")
+	                   .data("password", "rhwlfks!23")
+	                   .data("submit", "asdf")
+	                                   .method(Method.POST)
+	                   .execute();
+	           Document doc = Jsoup.connect("http://elearn.gwangju.ac.kr/index.jsp")
+	                   .cookies(loginResponse.cookies()).timeout(3000000).get();
+
+	                  
+	           System.out.println(doc);
+	       }
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 }
